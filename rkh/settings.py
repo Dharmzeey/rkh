5737b9c1f5b61bb0c797675c4ab755f06b48f3bf
Dharmzeey <olanrewajuadam2020@gmail.com>
2024-05-11 08:39:38 +0100

    railway

--- rkh/settings.py
+++ rkh/settings.py
@@ -18,10 +18,7 @@ import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 SECRET_KEY = 'django-insecure-yv+7!!ko(19d=$g)il+o(2h4vh1edhprb@#6oruvkgf!vz3s_!'
 
