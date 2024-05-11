fd750c66b8da576a2266012a1ffffec0216f6f9e
Dharmzeey <olanrewajuadam2020@gmail.com>
2024-05-11 07:58:01 +0100

    railway

--- rkh/settings.py
+++ rkh/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'rkh.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('PGDATABASE'),
         'USER':os.environ.get('PGUSER'),
         'PASSWORD':os.environ.get('PGPASSWORD'),
