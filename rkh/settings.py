fefe83defa42d87891a48b6f221b19f86cb723b3
Dharmzeey <olanrewajuadam2020@gmail.com>
2024-05-11 04:53:04 +0100

    first commit

--- rkh/settings.py
+++ rkh/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRETE_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECRET_KEY = 'django-insecure-yv+7!!ko(19d=$g)il+o(2h4vh1edhprb@#6oruvkgf!vz3s_!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
